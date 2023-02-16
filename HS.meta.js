546b7facda656a52a92dfca0a1ff3316cdc1f3a8
azzip <azzipstar@gmail.com>
2023-02-16 19:22:19 +0800

    ..

--- HS.meta.js
+++ HS.meta.js
@@ -2,7 +2,9 @@
 // @name HS 系統
 // @description HS 更新
 // @license MIT
-// @version  2.0.3
+// @version  2.0.4
+// @updateURL https://github.com/starazzip/slim_script/raw/main/HS.meta.js
+// @downloadURL https://github.com/starazzip/slim_script/raw/main/HS.user.js
 // @include *://3scasino.com/*
 // @grant GM.xmlHttpRequest
 // @namespace https://greasyfork.org/users/1028078
