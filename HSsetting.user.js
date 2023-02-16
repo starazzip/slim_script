7978771aee140138b1d5fc94435346285b71b5a9
azzip <azzipstar@gmail.com>
2023-02-16 19:28:17 +0800

    ..

--- HSsetting.user.js
+++ HSsetting.user.js
@@ -3,6 +3,8 @@
 // @description HS設定
 // @license MIT
 // @version  0.0.1
+// @updateURL https://github.com/starazzip/slim_script/raw/main/HSsetting.meta.js
+// @downloadURL https://github.com/starazzip/slim_script/raw/main/HSsetting.user.js
 // @include *://3scasino.com/*
 // @grant none
 // @namespace https://greasyfork.org/users/1028078
