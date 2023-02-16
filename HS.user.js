7fc5c4791864c97fa139f09f3a0023afddca9b77
azzip <azzipstar@gmail.com>
2023-02-16 19:07:45 +0800

    ..

--- HS.user.js
+++ HS.user.js
@@ -3,6 +3,8 @@
 // @description HS
 // @license MIT
 // @version  2.0.1
+// @updateURL https://github.com/starazzip/slim_script/raw/main/HS.meta.js
+// @downloadURL https://github.com/starazzip/slim_script/raw/main/HS.user.js
 // @include *://3scasino.com/*
 // @grant GM.xmlHttpRequest
 // @namespace https://greasyfork.org/users/1028078
