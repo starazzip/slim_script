5c61b562f8a36dd3fe0debdb1b243e7989eb53ef
azzip <azzipstar@gmail.com>
2023-02-16 17:17:56 +0800

    ..

---
+++ HSmeta.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name     HS reenter
+// @version  2.0.0
+// @grant    GM.xmlHttpRequest
+// @updateURL https://raw.githubusercontent.com/starazzip/slim_script/main/HS.txt
+// ==/UserScript==
