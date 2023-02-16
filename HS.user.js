92f68b08f8cb48001bbba2f53933e94638f45de9
azzip <azzipstar@gmail.com>
2023-02-16 19:04:59 +0800

    ..

--- HS.user.js
+++ HS.user.js
@@ -2,7 +2,7 @@
 // @name HS 系統
 // @description HS
 // @license MIT
-// @version  2.0.0
+// @version  2.0.1
 // @include *://3scasino.com/*
 // @grant GM.xmlHttpRequest
 // @namespace https://greasyfork.org/users/1028078
