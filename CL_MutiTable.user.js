80b37d6b29a49d7af241f721ff8ad14310bd693f
azzip <azzipstar@gmail.com>
2023-03-01 23:46:21 +0800

    ..

--- CL_MutiTable.user.js
+++ CL_MutiTable.user.js
@@ -34,6 +34,7 @@ document.onreadystatechange = function () {
   OnWebStateChanged();
 };
 
+
 function InsertButton() {
   var zNode = document.createElement('div');
   zNode.innerHTML = `<button id="myButton" type="button">Enter Room</button><br>`;
@@ -62,7 +63,7 @@ function OnWebStateChanged() {
             if (addn.classList.contains('tableWrap')) {
               clearTimeout(GoToMutiTableFailedMethod);
               HandleTableWrap(addn);
-            }           
+            }
           }
           if (addn.id === 'loginPage') {
             setTimeout(function () {
@@ -129,13 +130,41 @@ function HandleTableWrap(tableWrap) {
       console.log("保留: " + tableNameNode.innerText)
       tableWrap.style.height = '100px';
       tableWrap.style.backgroundColor = "#000000"
+      var tableName = tableWrap.querySelector(".tableName");
+      tableName.style.fontSize = '44px';
+      tableName.style.top = '50px'
+      var road = tableWrap.querySelector(".Baccarat8MultiTableRoadmap");
+      road.remove();
+      StartMonitorTotal(tableWrap);
     }
-    var tableName = tableWrap.querySelector(".tableName");
-    tableName.style.fontSize = '44px';
-    tableName.style.top='50px'
-    
-    var road = tableWrap.querySelector(".Baccarat8MultiTableRoadmap");
-    road.remove();
+  }, 3000);
+}
 
-  }, 1000);
+
+function StartMonitorTotal(tableWrap) {
+  var tableName = tableWrap.querySelector(".tableName");
+  let zNode = document.createElement('div');
+  zNode.setAttribute('id', 'myRound');
+	zNode.style.textAlign = 'left';
+  zNode.style.left = '41px';
+  zNode.style.color = "#FF0000"
+  zNode.style.fontSize = '44px'
+  tableName.parentNode.append(zNode);
+  let totalValueNode = tableWrap.querySelector(".detailContainer.evenBg.baccarat")
+    .querySelector(".detailTitle.yellow.Total")
+    .parentNode
+    .querySelector(".detailValue")
+  let TotalRoundObserver = new MutationObserver(mutations => {
+    mutations.forEach(function (mutation) {
+      zNode.innerText = mutation.target.innerText;
+      console.log("Total Monitor:");
+      console.log(mutation.target.innerText);
+    });
+  });
+
+  TotalRoundObserver.observe(totalValueNode, {
+    childList: true,
+    attributes: true,
+    characterData: true,
+  });
 }
