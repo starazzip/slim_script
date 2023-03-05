c973737b23d0b2f37bc9db6329aa02a14b953245
azzip <azzipstar@gmail.com>
2023-03-05 23:24:06 +0800

    ..

--- CL_MutiTableSetting.meta.js
+++ CL_MutiTableSetting.meta.js
@@ -1,6 +1,17 @@
 // ==UserScript==
 // @name     CL 多台設定
+// @description CL 多台的設定
+// @license MIT
 // @version  0.0.1
-// @grant    GM.xmlHttpRequest
+// @include *://www.cali111.net/*
+// @include *://www.cali222.net/*
+// @include *://www.cali333.net/*
+// @include *://www.cali444.net/*
+// @include *://www.cali555.net/*
+// @include *://www.cali666.net/*
+// @include *://www.cali777.net/*
+// @include *://www.cali888.net/*
+// @include *://www.cali999.net/*
+// @grant    none
 // ==/UserScript==
 //----------------------------------
--- CL_MutiTableSetting.user.js
+++ CL_MutiTableSetting.user.js
@@ -1,10 +1,21 @@
 // ==UserScript==
 // @name     CL 多台設定
+// @description CL 多台的設定
+// @license MIT
 // @version  0.0.1
+// @include *://www.cali111.net/*
+// @include *://www.cali222.net/*
+// @include *://www.cali333.net/*
+// @include *://www.cali444.net/*
+// @include *://www.cali555.net/*
+// @include *://www.cali666.net/*
+// @include *://www.cali777.net/*
+// @include *://www.cali888.net/*
+// @include *://www.cali999.net/*
 // @grant    none
 // ==/UserScript==
 //----------------------------------
-var host = "https://www.cali555.net/"
+var host = "https://www.cali888.net/"
 var username = "帳號"
 var password = "密碼"
 var room = [' Q001', ' Q002', ' Q003', ' Q004', ' Q005', ' B031', ' B001', ' B002', ' B018', ' B019', ' B301', ' B302', ' B303', ' B304', ' B305'];
