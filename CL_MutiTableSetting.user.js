bbc5274c3c25097da33c0d7661266ee942bfe36a
azzip <azzipstar@gmail.com>
2023-03-09 20:35:52 +0800

    ..

--- CL_MutiTableSetting.user.js
+++ CL_MutiTableSetting.user.js
@@ -2,22 +2,25 @@
 // @name     CL 多台設定
 // @description CL 多台的設定
 // @license MIT
-// @version  0.0.1
-// @include *://www.cali111.net/*
-// @include *://www.cali222.net/*
-// @include *://www.cali333.net/*
-// @include *://www.cali444.net/*
-// @include *://www.cali555.net/*
-// @include *://www.cali666.net/*
-// @include *://www.cali777.net/*
-// @include *://www.cali888.net/*
+// @version  0.0.2
 // @include *://www.cali999.net/*
+// @include *://www.cali888.net/*
+// @include *://www.cali777.net/*
+// @include *://www.cali666.net/*
+// @include *://www.cali555.net/*
+// @include *://www.cali333.net/*
+// @include *://www.cali1122.net/*
+// @include *://www.cali2233.net/*
+// @include *://www.calibet8.net/*
+// @include *://www.calibet9.net/*
+// @include *://www.cali1356a.net/*
+// @include *://www.cali1356b.net/*
 // @grant    none
 // ==/UserScript==
 //----------------------------------
 var host = "https://www.cali888.net/"
-var username = "帳號"
-var password = "密碼"
+var username = "6km3g"
+var password = "aaaa8888"
 var room = [' Q001', ' Q002', ' Q003', ' Q004', ' Q005', ' B031', ' B001', ' B002', ' B018', ' B019', ' B301', ' B302', ' B303', ' B304', ' B305'];
 
 
