9291bf961c4b3e866bda22eec87c68880a94e1e3
azzip <azzipstar@gmail.com>
2023-03-09 20:38:32 +0800

    ..

--- CL_MutiTable.user.js
+++ CL_MutiTable.user.js
@@ -2,16 +2,19 @@
 // @name     CL 多台系統-測試
 // @description CL 多台系統-測試中
 // @license MIT
-// @version  0.0.1
-// @include *://www.cali111.net/*
-// @include *://www.cali222.net/*
-// @include *://www.cali333.net/*
-// @include *://www.cali444.net/*
-// @include *://www.cali555.net/*
-// @include *://www.cali666.net/*
-// @include *://www.cali777.net/*
-// @include *://www.cali888.net/*
+// @version  0.0.2
 // @include *://www.cali999.net/*
+// @include *://www.cali888.net/*
+// @include *://www.cali777.net/*
+// @include *://www.cali666.net/*
+// @include *://www.cali555.net/*
+// @include *://www.cali333.net/*
+// @include *://www.cali1122.net/*
+// @include *://www.cali2233.net/*
+// @include *://www.calibet8.net/*
+// @include *://www.calibet9.net/*
+// @include *://www.cali1356a.net/*
+// @include *://www.cali1356b.net/*
 // @grant    GM.xmlHttpRequest
 // ==/UserScript==
 //----------------------------------
