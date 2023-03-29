9514ba2ae00449583c220357fc2dbf898ad939f4
azzip <azzipstar@gmail.com>
2023-03-29 21:36:47 +0800

    ..

--- HS_MutiTable.meta.js
+++ HS_MutiTable.meta.js
@@ -2,8 +2,9 @@
 // @name HS 多台系統
 // @description HS 多台
 // @license MIT
-// @version  0.0.1
+// @version  0.0.2
 // @include *://3scasino.com/*
+// @include *://play.kasar.live/*
 // @grant GM.xmlHttpRequest
 // @namespace https://greasyfork.org/users/1028078
 // ==/UserScript==
--- HS_MutiTableSetting.user.js
+++ HS_MutiTableSetting.user.js
@@ -2,8 +2,9 @@
 // @name HS 多台設定
 // @description HS 多台客制化設定
 // @license MIT
-// @version  0.0.1
+// @version  0.0.2
 // @include *://3scasino.com/*
+// @include *://play.kasar.live/*
 // @grant GM.xmlHttpRequest
 // @namespace https://greasyfork.org/users/1028078
 // ==/UserScript==
